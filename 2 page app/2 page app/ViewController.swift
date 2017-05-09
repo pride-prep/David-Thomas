1953aee2260d91d93c2cbcd177d023ae097fe9a5
Just-use-anything <david@my.prideprepschool.org>
2017-05-09 13:12:59 -0700

    multi view app

---
+++ 2 page app/2 page app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2 page app
+//
+//  Created by Student on 5/9/17.
+//  Copyright © 2017 David T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
